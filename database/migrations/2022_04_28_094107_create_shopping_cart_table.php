105056ee4b881f2312e15be12c8429c2e6c80339
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-04-28 11:48:36 +0200

    created shopping_cart_table and linked to user with one to one realtion

---
+++ database/migrations/2022_04_28_094107_create_shopping_cart_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShoppingCartTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shopping_cart', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shopping_cart', function (Blueprint $table) {
+            $table->dropForeign('shopping_cart_user_id_foreign');
+
+            $table->dropColumn('user_id');
+        });
+    }
+}
